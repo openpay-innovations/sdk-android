6246f89758c8a1839844bdfb35d86a7db28bfbd1
Kieran O'Brien <kieran.obrien@ittybittyapps.com>
2021-04-06 17:15:42 +1000

    Update button to use OPY assets for US

--- openpay/src/main/kotlin/au/com/openpay/sdkandroid/view/OpenpayButton.kt
+++ openpay/src/main/kotlin/au/com/openpay/sdkandroid/view/OpenpayButton.kt
@@ -5,10 +5,12 @@ import android.util.AttributeSet
 import androidx.annotation.ColorRes
 import androidx.appcompat.widget.AppCompatImageButton
 import androidx.core.content.res.use
+import androidx.core.os.LocaleListCompat
 import au.com.openpay.sdkandroid.R
 import au.com.openpay.sdkandroid.internal.coloredDrawable
 import au.com.openpay.sdkandroid.internal.dp
 import au.com.openpay.sdkandroid.internal.rippleDrawable
+import java.util.Locale
 
 private const val MIN_HEIGHT: Int = 48
 private const val MIN_WIDTH: Int = 218
@@ -63,7 +65,10 @@ class OpenpayButton @JvmOverloads constructor(
     private fun update() {
         setImageDrawable(
             context.coloredDrawable(
-                drawableResId = R.drawable.openpay_button_fg,
+                drawableResId = when (LocaleListCompat.getDefault()[0]) {
+                    Locale.US -> R.drawable.openpay_button_fg_opy
+                    else -> R.drawable.openpay_button_fg
+                },
                 colorResId = colorScheme.foregroundColorResId
             )
         )
