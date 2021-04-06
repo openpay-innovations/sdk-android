9de184e21f94696e538fdea522bc80aa78d9639d
Kieran O'Brien <kieran.obrien@ittybittyapps.com>
2021-04-06 17:09:43 +1000

    Update badge to use OPY assets for US

--- openpay/src/main/kotlin/au/com/openpay/sdkandroid/view/OpenpayBadge.kt
+++ openpay/src/main/kotlin/au/com/openpay/sdkandroid/view/OpenpayBadge.kt
@@ -5,13 +5,16 @@ import android.util.AttributeSet
 import androidx.annotation.ColorRes
 import androidx.appcompat.widget.AppCompatImageView
 import androidx.core.content.res.use
+import androidx.core.os.LocaleListCompat
 import au.com.openpay.sdkandroid.R
 import au.com.openpay.sdkandroid.internal.BackgroundDrawable
 import au.com.openpay.sdkandroid.internal.color
 import au.com.openpay.sdkandroid.internal.coloredDrawable
 import au.com.openpay.sdkandroid.internal.dp
+import java.util.Locale
 
 private const val MIN_WIDTH: Int = 75
+private const val MIN_WIDTH_OPY: Int = 40
 
 private const val PADDING_START: Int = 6
 private const val PADDING_TOP: Int = 6
@@ -39,7 +42,10 @@ class OpenpayBadge @JvmOverloads constructor(
 
     init {
         importantForAccessibility = IMPORTANT_FOR_ACCESSIBILITY_NO
-        minimumWidth = MIN_WIDTH.dp
+        minimumWidth = when (LocaleListCompat.getDefault()[0]) {
+            Locale.US -> MIN_WIDTH_OPY.dp
+            else -> MIN_WIDTH.dp
+        }
         setPaddingRelative(PADDING_START.dp, PADDING_TOP.dp, PADDING_END.dp, PADDING_BOTTOM.dp)
 
         context.theme.obtainStyledAttributes(attrs, R.styleable.OpenpayBadge, 0, 0)
@@ -59,7 +65,10 @@ class OpenpayBadge @JvmOverloads constructor(
     private fun update() {
         setImageDrawable(
             context.coloredDrawable(
-                drawableResId = R.drawable.openpay_logo_fg,
+                drawableResId = when (LocaleListCompat.getDefault()[0]) {
+                    Locale.US -> R.drawable.openpay_logo_fg_opy
+                    else -> R.drawable.openpay_logo_fg
+                },
                 colorResId = colorScheme.foregroundColorResId
             )
         )
